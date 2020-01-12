2b6d1a45f110da303bde2834ec2ea13703bbbff9
jch2617 <jch2617@gmail.com>
2020-01-11 21:03:19 -0500

    first commit for lab 3

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const name = 'Mitch Cuckovich';
 const age = '25';
 const birthday = 'January 24.';
-const detroitGC = true;
+const detroitGC = false;
 
 if (detroitGC === true) {
-    console.log('My name is ' + name + ' and I am a student at Grand Circus in Detroit, Michigan. I am currently ' + age +  ' years old and my birthday is on ' + birthday);
+    console.log(`My name is ${name}  and I am a student at Grand Circus in Detroit, Michigan. I am currently ${age} years old and my birthday is on ${birthday}`);
 } else {
-    console.log('My name is ' + name + ' and I am a student at Grand Circus in Grand Rapids, Michigan. I am currently ' + age + ' years old and my birthday is on ' + birthday);
+    console.log(`My name is ${name} and I am a student at Grand Circus in Grand Rapids, Michigan. I am currently ${age} years old and my birthday is on ${birthday}`);
 }
 
 let lifeEvents = ['I was born in Troy, Michigan.', 'I went to Hope College', 'I went to Junior Olympics when I was 10 years old. I placed 14th in the nation in the 800 meter event.', "I'm a graduate of the front-end bootcamp."];
@@ -19,12 +19,11 @@ let counter = 0;
 
 while (true) {
     let randomNumber = Math.floor(Math.random() * (11 - 1) + 1);
-if (randomNumber !==5) {
+    counter++;  
+  if (randomNumber !==5) {
   console.log(randomNumber + "!==5");
-  counter++;
 } else {
   console.log("5 === 5. It took " + counter + " iterations to randomly generate the number 5");
-  counter++;
   break;
 }
 }
